16715c2e794733118455758604f559a881e275d4
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2023-09-30 20:56:00 +0600

    Delete Lab01/086.cpp

--- Lab01/086.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-cout<<endl<<endl<<"Hello in my University"<<endl<<endl<<endl<<"I am a student in it faculty"<<endl<<endl<<endl<<"Welcome"<<endl<<endl;
-return 0;
-}
